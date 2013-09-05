da4c6a63c781b679e8128848678141b36558c9ca
Patrick Ruhkopf <patrickruhkopf@gmail.com>
2013-09-05 00:07:52 -0400

    initial commit

---
+++ src/main/java/name/ruhkopf/spring/thymeleaf/controller/VersionController.java
@@ -0,0 +1,18 @@
+package name.ruhkopf.spring.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class VersionController
+{
+
+	@RequestMapping("/helloWorld")
+	public String helloWorld(Model model)
+	{
+		model.addAttribute("message", "Hello World!");
+		return "version";
+	}
+}
